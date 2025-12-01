7155271836519f1af3835fd70520ff900bcd268c
Goutam-4-GitHub <gautam.prasad0424@gmail.com>
2025-12-01 09:33:01 +0530

    Allow Netlify URL in CORS

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173', // your React frontend URL
+    origin: 'clothing-ecommerce-netlify.netlify.app', // your React frontend URL
     credentials: true,
   })
 );
